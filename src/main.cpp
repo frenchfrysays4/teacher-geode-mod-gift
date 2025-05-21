2dd9366080fd4bf583a694edea6743d3d7d4dac1
frenchfrysays4 <lucas.fry@techie.com>
2025-05-21 13:25:21 -0500

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <Geode/Geode.hpp>
+
+/**
+ * This is a Mod for Mr/Ms/Mrs. [teacher's name].
+ * He/She thought up the mod idea.
+ * I coded it into a Geode mod.
+*/
+
+// Start writing code here \/
